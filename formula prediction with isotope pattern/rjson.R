f472d5a95a6a24644c8cdb5363fb36104d59b38b
LiChenPU <35385956+LiChenPU@users.noreply.github.com>
2019-03-12 15:17:13 -0400

    test

--- formula prediction with isotope pattern/rjson.R
+++ formula prediction with isotope pattern/rjson.R
@@ -4,9 +4,10 @@ library(dplyr)
 #test2
 
 target=906.9958
+
 massRange = 5*906.9958/10^6
 time = Sys.time()
-mfRange="C0-100H0-100N0-12O0-20S0-5P0-5S0-5"
+mfRange="C0-99 H0-100 N0-12 O0-20 P0-3 S0-3 Si0-2 K0-1 Cl0-5 Na0-1"
 
 query <- paste("http://www.chemcalc.org/chemcalc/em?monoisotopicMass=",target,
                "&mfRange=", mfRange,
@@ -18,17 +19,14 @@ resJSON <- fromJSON(paste(readLines(query), collapse=""))
 
 temp = data.frame(mf="mf", em = 0, error = 0, stringsAsFactors = F)
 temp_ls = list()
-for(i in 1:1){
+
 for (mf.index in 1:length(resJSON$results)) {
-  
-  temp[1,] = list( "mf"=resJSON$results[[mf.index]]$mf , 
-                         "em"=resJSON$results[[mf.index]]$em, 
+
+  temp[1,] = list( "mf"=resJSON$results[[mf.index]]$mf ,
+                         "em"=resJSON$results[[mf.index]]$em,
                          "error"=resJSON$results[[mf.index]]$error )
   temp_ls[[mf.index]]=temp
-}
 }
 formula_ls2 = bind_rows(temp_ls)
 time = Sys.time()-time
 time
-  
-
