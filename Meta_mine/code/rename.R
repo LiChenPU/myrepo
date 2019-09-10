76276412696905b72bd343575e8410b7835d99ab
LiChenPU <35385956+LiChenPU@users.noreply.github.com>
2019-09-10 15:31:32 -0400

    minor

--- Meta_mine/code/rename.R
+++ Meta_mine/code/rename.R
@@ -1,23 +1,13 @@
 
 
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
-setwd("./Full Scans")
-filenames = list.files(pattern=".mzXML")
+setwd("C:/study/data/exactive/Yeast_unknown/190906 Yeast unknowns + spike in")
+filenames = list.files(pattern="13C_d7NH4OAc", recursive = T)
 
 
 for(i in filenames){
   newname = i
-  newname = sub("336", "KO_336_M", newname)
-  newname = sub("339", "KO_339_F", newname)
-  newname = sub("340", "KO_340_F", newname)
-  newname = sub("337", "Het_337_M", newname)
-  newname = sub("341", "Het_341_F", newname)
-  newname = sub("342", "Het_342_F", newname)
-  newname = sub("347", "Het_347_M", newname)
-  newname = sub("338", "WT_338_M", newname)
-  newname = sub("345", "WT_345_F", newname)
-  newname = sub("348", "WT_348_M", newname)
-  
+  newname = gsub("13C_d7NH4OAc(.*)","12C_d7NH4OAc_M\\1",i)
   
   file.rename(i, newname)
 }
--- formula prediction with isotope pattern/rjson.R
+++ formula prediction with isotope pattern/rjson.R
@@ -3,12 +3,14 @@ library(dplyr)
 
 #test2
 
-target=83.904
+target=162.8927
 
-charge = ""
+charge = "-"
 massRange = 25*target/10^6
 time = Sys.time()
-mfRange="C0-99 H0-100 N0-12 O0-20 P0-3 S0-3 Si0-2 K0-1 Cl0-5 Na0-1 Cu0-1 Cr0-1"
+# mfRange="C8-99 H15-100 N1-12 O6-20 P1-3 S0-3 B1-1 Si1-2 K0-3 Cl0-5 Na0-3 Cu0-1 Cr0-1 Ca0-1 Ni0-1"
+# mfRange="C7-20 H0-100 N0-7 O0-20 P0-3 S0-3 Si0-2 K1-4 Cl0-5 Na0-3 Cu0-1 Cr0-1 Ca0-1 Ni0-1 B0-1 Fe1-1"
+mfRange="C1-20 H0-100 N0-7 O1-20 P0-3 S0-3 Si0-2 K0-4 Cl0-5 Na0-3 Cu0-1 Cr0-1 Ca0-1 Ni0-1 B0-1 Li0-1 Fe0-1 Mg0-1 Be0-1 F0-6 Al0-1"
 mfRange = paste(mfRange, charge, sep="")
 
 query <- paste("http://www.chemcalc.org/chemcalc/em?monoisotopicMass=",target,
