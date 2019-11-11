54fbc6ad267cc6a4c24645dc410baf4444ee0ac9
LiChenPU <35385956+LiChenPU@users.noreply.github.com>
2019-11-11 16:14:34 -0500

    minor add timestamp and log

--- NetID/pubchem/Pubchem_SDFtoRDS.R
+++ NetID/pubchem/Pubchem_SDFtoRDS.R
@@ -3,9 +3,12 @@ library(ChemmineR)
 library(dplyr)
 
 filenames = list.files(pattern = ".sdf.gz")
+
+sink("log.txt")
+print(timestamp())
 for(i in 1:length(filenames)){
   if(file.exists(sub("sdf.gz","rds", filenames[i]))){next} # skip when rds file exists
-  
+  print(paste("Processing", filenames[i]))
   dsfset = read.SDFset(filenames[i])
   valid <- validSDF(dsfset) # Identifies invalid SDFs in SDFset objects 
   dsfset <- dsfset[valid] # Removes invalid SDFs, if there are any 
@@ -16,3 +19,4 @@ for(i in 1:length(filenames)){
   saveRDS(temp_result, sub("sdf.gz","rds", filenames[i]))
 }
 
+sink()
