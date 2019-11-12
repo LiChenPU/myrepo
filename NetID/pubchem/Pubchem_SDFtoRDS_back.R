6f834d5432f648407cabc8a275209f623fe5a39b
LiChenPU <35385956+LiChenPU@users.noreply.github.com>
2019-11-12 17:15:28 -0500

    update backward

---
+++ NetID/pubchem/Pubchem_SDFtoRDS_back.R
@@ -0,0 +1,22 @@
+# BiocManager::install("ChemmineR")
+library(ChemmineR)
+library(dplyr)
+
+filenames = list.files(pattern = ".sdf.gz")
+
+sink("log_back.txt")
+print(timestamp())
+for(i in length(filenames):1){
+  if(file.exists(sub("sdf.gz","rds", filenames[i]))){next} # skip when rds file exists
+  print(paste("Processing", filenames[i]))
+  dsfset = read.SDFset(filenames[i])
+  valid <- validSDF(dsfset) # Identifies invalid SDFs in SDFset objects 
+  dsfset <- dsfset[valid] # Removes invalid SDFs, if there are any 
+  
+  temp_result_ls = datablock(dsfset)
+  temp_result = rbind_list(temp_result_ls)
+  
+  saveRDS(temp_result, sub("sdf.gz","rds", filenames[i]))
+}
+
+sink()
