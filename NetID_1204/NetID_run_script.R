67b78721f8a127d0849e379d1fa8aaa37ffb02e2
LiChenPU <35385956+LiChenPU@users.noreply.github.com>
2020-04-13 15:56:34 +0800

    implement BMW output format

--- NetID_1204/NetID_run_script.R
+++ NetID_1204/NetID_run_script.R
@@ -2,19 +2,19 @@
 ## Read files ####
 
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+printtime = Sys.time()
+timestamp = paste(unlist(regmatches(printtime, gregexpr("[[:digit:]]+", printtime))),collapse = '')
 source("NetID_function.R")
 
-work_dir = "Lin_Yeast_Neg"
+work_dir = "WL_liver_neg"
+# work_dir = "Lin_Yeast_Neg"
 ion_mode = -1
-Empirical_rules_file = "./dependent/Empirical_rules.csv"
-
 MassDistsigma = 0.5
 LC_method = "Hilic_25min_QE"
+Empirical_rules_file = "./dependent/Empirical_rules.csv"
 print(work_dir)
 print(ion_mode)
 
-printtime = Sys.time()
-timestamp = paste(unlist(regmatches(printtime, gregexpr("[[:digit:]]+", printtime))),collapse = '')
 # sink(paste(timestamp,"log.txt"))
 # sink()
 
@@ -27,18 +27,39 @@ timestamp = paste(unlist(regmatches(printtime, gregexpr("[[:digit:]]+", printtim
   
   Mset[["Empirical_rules"]]=Read_rule_table(rule_table_file = Empirical_rules_file)
   
+  Mset[["Global_parameter"]]=  list(mode = ion_mode,
+                                    LC_method = LC_method)
+}
+
+
+## WL data format ####
+{
   setwd(work_dir)
+  
   filename = "raw_data.csv"
   Mset[["Raw_data"]] <- read_raw_data(filename)
-  manual_library_file = "manual_library.csv"
   
-  Mset[["Manual_library"]] = read_manual_library(manual_library_file)
+  filename_wl = "pks_liver_neg_buff_2020-02-21.xlsx"
+  WL = readxl::read_xlsx(filename_wl) %>%
+    dplyr::rename(medRt = rt,
+                  medMz = mz)
+  
+  test = Mset$Raw_data %>%
+    merge(WL, all = T) %>%
+    mutate(id = 1:nrow(.),
+           liver = 10^sig,
+           liver2 = 10^sig) %>%
+    dplyr::select(colnames(Mset$Raw_data))
+  Mset[["Raw_data"]] = test
+
 }
+
 
 ## Initialise ####
 {
-  Mset[["Global_parameter"]]=  list(mode = ion_mode,
-                                    LC_method = LC_method)
+  manual_library_file = "manual_library.csv"
+  Mset[["Manual_library"]] = read_manual_library(manual_library_file)
+
   Mset[["Cohort"]]=Cohort_Info(Mset, first_sample_col_num = 15)
   print(Mset$Cohort)
   
@@ -46,11 +67,18 @@ timestamp = paste(unlist(regmatches(printtime, gregexpr("[[:digit:]]+", printtim
   Mset[["Data"]] = Peak_cleanup(Mset,
                                 mz_tol=1/10^6, 
                                 rt_tol=0.1,
-                                inten_cutoff=0,
-                                high_blank_cutoff = 0,
+                                inten_cutoff=1e4,
+                                high_blank_cutoff = 2,
                                 first_sample_col_num = 15)
+  
   print(c(nrow(Mset$Raw_data), nrow(Mset$Data)))
+  
+  manual_library_file = "manual_library.csv"
+  Mset[["Manual_library"]] = read_manual_library(manual_library_file)
+  
+  
 }
+
 
 ## Initiate nodeset and edgeset ####
 {
@@ -130,9 +158,6 @@ save.image()
   
 save.image(paste0(timestamp,".RData"))
 
-print("total run time")
-print(Sys.time()-printtime)
-
 ## Run_cplex ####
 {
   CplexSet[["init_solution"]] = list(Run_cplex(CplexSet, obj_cplex = CplexSet$para$obj))
@@ -159,15 +184,43 @@ print(Sys.time()-printtime)
   
   test = ilp_nodes %>%
     filter(ilp_result > 0.01 | is.na(ilp_result))
+  test2 = merge(WL, test, by.x = "Index", by.y = "Input_id", all.x = T) %>%
+    dplyr::select(colnames(WL), path, formula, category, parent_formula, node_id, parent_id)
+  
+  write_csv(test2, "WL_neg_NetID.csv", na="")
+  
+  test3 = test2 %>%
+    filter(is.na(Background)) %>%
+    # dplyr::select(colnames(WL)[colnames(WL)!="Index"], formula, everything()) %>%
+    mutate(Formula = ifelse(is.na(Formula), "", Formula)) %>%
+    mutate(Formula = check_chemform(isotopes, Formula)$new_formula)
+           
+  test3_filter = test3 %>%
+    filter(sig > 5) %>%
+    # filter(Feature == "BuffSS") %>%
+    # filter(Feature != "Metabolite") %>%
+    # filter(category == "Metabolite") %>%
+    # filter(!(Feature == "Isotope" & category == "Artifact")) %>%
+    # filter(Formula == formula & Formula != "") %>%
+    filter(!is.na(formula)) %>%
+    filter(T)
+  
+  library(janitor)
+  crosstable = tabyl(test3, Feature, category)
+  
+  
 }
 
 ## View Results ####
 {
   result = FormulaSet_df %>%
+    mutate(temp_cat = ifelse(category == "Metabolite", "Metabolite", "Artifact")) %>%
     merge(ilp_nodes %>% 
-            dplyr::select(node_id, formula, category, ilp_node_id, ilp_result, cplex_score), 
+            mutate(temp_cat = ifelse(category == "Metabolite", "Metabolite", "Artifact")) %>%
+            dplyr::select(node_id, formula, temp_cat, ilp_node_id, ilp_result, cplex_score), 
           all.x = T) %>%
     # mutate(node_id = factor(node_id, levels = 1:length(NodeSet))) %>%
+    dplyr::select(-temp_cat) %>%
     split(.$node_id)
   
   result_ls = vector("list", length(NodeSet))
@@ -177,19 +230,24 @@ print(Sys.time()-printtime)
     } 
   }
   
-
-  tictoc::tic()
-  for(i in unique(ilp_nodes$node_id)){
-    # test = sapply(, query_path, result_ls, LibrarySet)
-    test = query_path(i, result_ls, LibrarySet)
-    print(i)
-  }
-
-  tictoc::toc()
   
+  node_path = sapply(unique(ilp_nodes$node_id), query_path, result_ls, LibrarySet)
+  path_annotation = data.frame(node_id = unique(ilp_nodes$node_id),
+                               path = node_path)
+
+  ilp_nodes = ilp_nodes %>%
+    merge(path_annotation) %>%
+    dplyr::select(path, everything()) 
   
+  ilp_nodes2 = ilp_nodes %>%
+    filter(ilp_result > 0.01 | is.na(ilp_result)) %>%
+    filter(grepl("Thiamine", path))
+  
+  save(ilp_nodes, ilp_edges, heterodimer_ilp_edges, result_ls, file="network.RData")
 }
 
+print("total run time")
+print(Sys.time()-printtime)
 
 # ## PAVE evaluation ####
 {
@@ -202,11 +260,9 @@ print(Sys.time()-printtime)
     dplyr::select(ID...1,
                   c(6:7,68:ncol(.)))
 
-  id_Input_id_map = Mset$Data %>%
-    dplyr::select(id, Input_id)
-
   merge_result = ilp_nodes %>%
     merge(groundtruth, by.x="Input_id", by.y="ID...1")
+    
 
   merge_result = merge_result %>%
     mutate(formula_match = formula == Formula...68)
@@ -214,19 +270,22 @@ print(Sys.time()-printtime)
   merge_result_filter = merge_result %>%
     filter(ilp_result > 0.01 | is.na(ilp_result)) %>%
     filter(formula_match) %>%
+    # filter(!is.na(Formula_validated) | Formula_validated == "Y") %>%
     filter(!is.na(Formula_validated) | Formula_validated == "Y", Formula_validated != "?") %>%
     # arrange(-ilp_result) %>%
     # distinct(node_id, .keep_all=T) %>% 
     # filter(ilp_result < 0.01) %>% 
     # filter(!formula_match) %>%
+    dplyr::select(path, everything()) %>%
+    filter(Status_validated == "Adduct", category == "Metabolite")
     filter(T)
   
-  test = tabyl(merge_result_filter, category, Status_validated)
+  # test = tabyl(merge_result_filter, category, Status_validated)
   
-  merge_result_filter2 = merge_result_filter %>%
-    filter(category == "Metabolite" | Status_validated == "Metabolite", 
-           category != Status_validated) %>%
-    filter(T)
+  # merge_result_filter2 = merge_result_filter %>%
+  #   filter(category == "Metabolite" | Status_validated == "Metabolite", 
+  #          category != Status_validated) %>%
+  #   filter(T)
 }
 
 ## Query specific nodes or edges
