98e348d88a2765f6088cafd5b549c7011f469830
LiChenPU <35385956+LiChenPU@users.noreply.github.com>
2020-05-15 16:34:12 +0800

    major rework on shiny visualization

--- NetID_1204/R_shiny_App.R
+++ NetID_1204/R_shiny_App.R
@@ -1,66 +1,77 @@
-# Load library ####
-{
-  library(shiny)
-  library(igraph)
-  library(reactlog)
-  library(ShinyTester)
-  # install.packages("visNetwork")
-  library(visNetwork)
-  library(dplyr)
-  library(RColorBrewer)
-  library(stringr)
-}
 
-# options(digits = 8)
-# 
-# test_edge = g_edge %>% filter(ILP_result !=0, grepl("\\[37", linktype))
-# test_node = g_vertex %>% filter(ID == 1156)
-# {
-#   select_node_id = 177
-#   select_formula_ls = formula_list %>%
-#     filter(ID == select_node_id)
-# 
-# }
-# 
-# {
-#   select_ilp_id = 14051
-#   select_ilp_id = 359
-#   select_edge_info_sum = g_edge %>%
-#     filter(from == select_ilp_id | to == select_ilp_id)
-# 
-# }
-# 
-# {
-#   pred_formula_ls = CPLEXset$formula$pred_formula_ls
-#   pred_formula_ls[[1797]]
-#   pred_formula_ls[[5109]]
-# }
-
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+source('R_shiny_functions.R')
+source("NetID_function.R")
 
 # options(shiny.reactlog=TRUE) # Enable reactlog here
+# options(digits = 8)
+
 # Read in files ####
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 datapath = ("./Xi_new_neg")
 setwd(datapath)
 
-g_vertex = read.csv("g_vertex2.txt", stringsAsFactors = F) %>%
-  mutate(mz = round(mz, 4)) %>%
-  mutate(RT = round(RT, 2)) %>%
-  mutate(intensity = signif(intensity, 6))
-g_edge = read.csv("g_edge2.txt", stringsAsFactors = F)
+# Prepare 
+{
+  dt = readRDS("output.rds")
+  
+  g_met = initiate_g_met(dt$ilp_nodes, dt$ilp_edges)
+  
+  core_met = dt$ilp_nodes %>%
+    filter(steps == 0) %>%
+    filter(class == "Metabolite")
+  
+  core_nonmet = dt$ilp_nodes %>%
+    filter(steps %% 1 == 0) %>%
+    filter(class != "Unknown")
+  
+  g_nonmet = initiate_g_nonmet(dt$ilp_nodes, dt$ilp_edges, dt$heterodimer_ilp_edges)
+  
+  ilp_edges_annotate_met = igraph::as_data_frame(g_met, "edges")
+  ilp_edges_annotate_nonmet = igraph::as_data_frame(g_nonmet, "edges")
+  
+  test = dt$ilp_edges %>%
+    filter(category == "Library_MS2_fragment")
+}
 
-g <- graph_from_data_frame(d = g_edge, vertices = g_vertex, directed = T)
-g_vertex = igraph::as_data_frame(g, "vertices")
-g_edge = igraph::as_data_frame(g, "edges")
 
-# test_g = search_partner(g, peak_id = 122, formula_select = "C4H7K3O8S1", step=1)
-# test_g = search_partner(g, peak_id = 127, formula_select = "C20H32N6O12S2", step=1)
-# test_g = search_partner(g, peak_id = 178, formula_select = "C6H12O6", step=1)
+# function
+{
+  # given a metabolite ilp
+  # show all shortest connections of from core
+  # for each core, show all possible formulas
+  g_met_interest = network_annotation_met(query_ilp_id = 34, 
+                                          g_annotation = g_met, 
+                                          core_ilp_node = core_met,
+                                          optimized_only = T)
+  Plot_g_interest(g_met_interest, query_ilp_node = 34)
+  
+  g_artifact_interest = network_annotation_nonmet(query_ilp_id = 523,  #2863
+                                                  g_annotation = g_nonmet, 
+                                                  core_ilp_node = core_nonmet, 
+                                                  weight_tol = 3,
+                                                  optimized_only = T)
+  Plot_g_interest(g_artifact_interest, query_ilp_node = 523)
+  
+  g_child_artifact = network_child_nonmet(query_ilp_id = 2, 
+                                          g_annotation = g_nonmet,
+                                          connect_degree = 1,
+                                          optimized_only = T)
+  Plot_g_interest(g_child_artifact, query_ilp_node = 2)
+  
+  
+  core_rank = core_annotate(dt$ilp_nodes, dt$FormulaSet_df, dt$LibrarySet)
+  
+  
+
+}
+
+
+
 
 
 ## Run shiny ####
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
-source('R_shiny_functions.R')
 source('R_shiny_UI.R', local = TRUE)
 source('R_shiny_Server.R')
 shinyApp(ui = ui, server = server)
